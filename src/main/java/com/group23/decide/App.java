2c79a83a969c44f6dd477f58bcd69baa3225d19f
Calin Capitanu <calincapitanu@gmail.com>
2022-01-26 11:57:10 +0100

    Add task pdf and maven create (#1)

---
+++ src/main/java/com/group23/decide/App.java
@@ -0,0 +1,13 @@
+package com.group23.decide;
+
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
