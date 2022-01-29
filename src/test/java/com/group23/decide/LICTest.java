6bfda6c6a84415bd90b7b65e8699db59ca6f4cc2
ArnPellesGit <pelle.arn@gmail.com>
2022-01-29 21:08:01 +0100

    #12 added basic tests for condition 8

--- src/test/java/com/group23/decide/LICTest.java
+++ src/test/java/com/group23/decide/LICTest.java
@@ -141,4 +141,55 @@ public class LICTest {
         assertFalse(LIC.condition5(2, new Point[] { p1, p2 }));
     }
 
+    // /**
+    // * Testing that CMV0 works on a trivial case
+    // */
+    // @Test
+    // public void cmv8TrivialWorks() {
+    // Point p0 = new Point(0, 0);
+    // // Sending in the same point twice should return false since there aren't
+    // // two points with a distance of Length apart
+    // assertFalse(LIC.condition0(2, new Point[] { p0, p0 }, 1));
+    // }
+
+    /**
+     * Testing that CMV8 works on a trivial case
+     */
+    @Test
+    public void cmv8TrivialWorks() {
+        // Only one point is not enough for this to work => should return false
+        Point p0 = new Point(0, 0);
+        assertFalse(LIC.condition8(1, new Point[] { p0 }, 1, 1, 3));
+    }
+
+    /**
+     * Testing that CMV8 accepts inputs expected to be valid
+     */
+    @Test
+    public void cmv8AcceptWorks() {
+        Point p0 = new Point(0, 0);
+        Point p1 = new Point(1, 1);
+        Point p2 = new Point(2, 2);
+        Point p3 = new Point(3, 3);
+        Point p4 = new Point(4, 4);
+        // Distance from (0,0) -> (4,4) is sqrt(32)
+        // For dist above to be > RADIUS1*2 => Should have a radius of at least 3
+        assertTrue(LIC.condition8(5, new Point[] { p0, p1, p2, p3, p4 }, 1, 1, 3));
+    }
+
+    /**
+     * Testing that CMV8 accepts inputs expected to be valid
+     */
+    @Test
+    public void cmv8RejectWorks() {
+        Point p0 = new Point(0, 0);
+        Point p1 = new Point(1, 1);
+        Point p2 = new Point(2, 2);
+        Point p3 = new Point(3, 3);
+        Point p4 = new Point(4, 4);
+        // Distance from (0,0) -> (4,4) is sqrt(32)
+        // For dist above to be > RADIUS1*2 => Should have a radius of at least 3 to succeed
+        assertFalse(LIC.condition8(5, new Point[] { p0, p1, p2, p3, p4 }, 1, 1, 2));
+    }
+
 }
